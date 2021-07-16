8f8349ecb5fe11dbf574aa2d346ac91051e61228
Gabriel Bernardino <79287503+Gaabrielbernardino@users.noreply.github.com>
2021-07-15 23:19:01 -0300

    Add files via upload

---
+++ src/Components/Photo/Photo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { PHOT_GET } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Loading';
+import PhotoContent from './PhotoContent';
+
+const Photo = () => {
+  const {id} = useParams();
+  const {data , loading , error , request} = useFetch();
+
+  React.useEffect(()=> {
+    const {url , options} = PHOT_GET(id);
+    request(url , options);
+  }, [request,id]);
+
+  if(error)  return <Error error={error}/>
+  if(loading)  return <Loading/>
+  if(data)  return <section><PhotoContent data={data}/></section>
+else return null 
+};
+
+export default Photo;
